f7f08a2ae035869bd12abdcddbd6c53308984721
Oliver Kuhn <oliver.kuhn@web.de>
2017-09-23 13:16:18 +0200

    add tag phlogratos and build date

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -6,9 +6,9 @@
 #define NODE_VERSION_REVISION	0U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU 2.1.0"
+#define NODE_VERSION	"NodeMCU 2.1.0+phlogratos"
 #ifndef BUILD_DATE
-#define BUILD_DATE	  "unspecified"
+#define BUILD_DATE	  "20170923"
 #endif
 
 extern char SDK_VERSION[];
